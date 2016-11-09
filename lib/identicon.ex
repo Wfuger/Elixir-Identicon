7fce91678a890a6c471f03a51572b01f28125166
Wfuger <wfuger@gmail.com>
2016-11-09 14:57:19 -0600

    mirror row, flatten nested lists

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -12,10 +13,22 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    # Pattern match the image and grab first 3 values of the hex list
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  # Pattern match the image and grab first 3 values of the hex list
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-    [r, g, b]
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1) #&func_name/argument number is passing a referrence of function
+    |> List.flatten
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    # ++ is append to list operator
+    row ++ [second, first]
   end
 end
